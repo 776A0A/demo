c7337a68cbc40ac2d48117ff08391ab475632b6a
har3dy <har3dy@icloud.com>
2019-08-21 19:44:28 +0800

    初步完成图片上传轮子和点击图片放大轮子

--- js/utils.js
+++ js/utils.js
@@ -25,7 +25,7 @@ const utils = {
       this.wrapper = document.getElementById(id);
       this.prevClickedElem = new WeakMap(); // 用于存储用户上一次点击的元素
       let template = `<img src=${this.image_1} alt="star" class="star">`; // 基本模板
-      let html = template
+      let html = template;
       for (let i = 1; i < total; i++) {
         html += template;
       }
@@ -51,6 +51,7 @@ const utils = {
         if (currentSrc === image_1) {
           let _currentElem = e.target;
           _currentElem.setAttribute('src', image_2)
+          // TODO 在处理这个的时候能不能封装个函数？
           while (currentElem = currentElem.previousSibling) {
             currentElem.setAttribute('src', image_2)
           }
@@ -75,6 +76,106 @@ const utils = {
       })
     }
   },
+  _uploadImage: class {
+    constructor(className, blankImage, url) {
+      this.className = className;
+      this.blankImage = blankImage;
+      this.url = url;
+    }
+    init() {
+      let wrapper = [...document.getElementsByClassName(this.className)];
+      let input = `
+      <img class="uploadedImage" src="./images/plain.png" alt="uploadedImage" style="display: none;"> 
+      <div id="blank-image">
+        <input type="file" name="" id="uploadImage" accept="image/*">
+        <img src="${this.blankImage}" alt="图片" class="blank-image">
+      </div>
+    `;
+    }
+  },
+  /**
+   * 图片上传功能并实现预览和点击图片放大
+   * @param {*} className 包裹元素的类名
+   * @param {*} blankImage 默认显示的图片
+   * @param {*} url 上传图片的接口
+   */
+  uploadImage(className, blankImage, url) {
+    let wrapper = [...document.getElementsByClassName(className)];
+    let input = `
+      <img class="uploadedImage" src="./images/plain.png" alt="uploadedImage" style="display: none;"> 
+      <div id="blank-image">
+        <input type="file" name="" id="uploadImage" accept="image/*">
+        <img src="${blankImage}" alt="图片" class="blank-image">
+      </div>
+    `;
+    wrapper.forEach(elem => {
+      elem.innerHTML = input;
+      elem.style.cssText += `display: flex; align-items: center; display: flex; align-items: center; font-size: 0; flex-flow: row wrap;`;
+      let imgs = [...document.querySelectorAll(`.${className} img`)];
+      imgs.forEach(img => {
+        img.style.cssText += `width: 5rem; height: 5rem; margin: .5rem; margin-left: 0;`;
+      })
+      let clickToUpload = document.getElementById('uploadImage');
+      clickToUpload.addEventListener('change', e => {
+        let image = e.currentTarget.files[0];
+        let imageUrl = URL.createObjectURL(image);
+        let newImg = document.createElement('img');
+        newImg.classList.add('uploadedImage')
+        newImg.src = imageUrl;
+        newImg.setAttribute('alt', 'uploadedImage')
+        newImg.style.cssText += `width: 5rem; height: 5rem; margin: .5rem; margin-left: 0;`;
+        newImg.addEventListener('click', e => {
+          let preview = document.getElementById('preview');
+          let elem = e.currentTarget;
+          preview.innerHTML = `<img src=${elem.src}  id="fullImage" />`;
+          preview.style.display = 'block';
+          let fullImage = document.getElementById('fullImage')
+          fullImage.style.cssText += `position: absolute; width: 100%; top: 50%; transform: translateY(-50%); max-height: 100%;`
+          function handlePreviewClick(e) {
+            preview.style.display = 'none';
+            preview.removeEventListener('click', handlePreviewClick)
+          }
+          preview.addEventListener('click', handlePreviewClick)
+        })
+        let uploadedImage = document.getElementsByClassName('uploadedImage')[0];
+        elem.insertBefore(newImg, uploadedImage)
+        // let file = new File()
+        // file.append('image', image)
+        // fetch(url, {
+        //   method: 'POST',
+        //   body: file
+        // }).then(res => {
+        //   if (res.ok) {
+
+        //   } else {
+
+        //   }
+        // }).catch(err => { })
+      })
+    })
+  },
+  /**
+   * 点击图片放大
+   * @param {*} id 图片列表外层的包裹元素 id
+   * @param {*} previewId 预览图的 wrapper
+   */
+  fullScreen(id, previewId) {
+    let preview = document.getElementById(previewId);
+    let wrapper = document.getElementById(id);
+    wrapper.addEventListener('click', e => {
+      let img = e.target;
+      if (img.tagName !== 'IMG') return;
+      preview.innerHTML = `<img src=${img.src} id="fullImage" />`;
+      preview.style.display = 'block';
+      let displayImage = document.getElementById('fullImage');
+      displayImage.style.cssText += `position: absolute; width: 100%; top: 50%; transform: translateY(-50%);`
+      preview.addEventListener('click', handlePreviewClick)
+    })
+    function handlePreviewClick(e) {
+      preview.style.display = 'none';
+      preview.removeEventListener('click', handlePreviewClick)
+    }
+  }
 }
 
 export default utils
