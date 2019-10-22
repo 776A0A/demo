bd00a41d40699b78eab27649d516833af1c3b7f0
har3dy <har3dy@icloud.com>
2019-10-22 14:54:51 +0800

    练习输入 email 功能

---
+++ lesson/lesson-3-input-email.js
@@ -0,0 +1,101 @@
+window.onload = function () {
+  document.getElementById('email-input').focus()
+}
+class InputHandler {
+  constructor() {
+    this.postfixList = ['163.com', 'gmail.com', '126.com', 'qq.com', '263.net'];
+    this.input = document.getElementById('email-input'),
+      this.ul = document.getElementById('email-sug-wrapper');
+    this.isTyping = false;
+    this.init()
+  }
+  init() {
+    this.bindEvent()
+    this.handleLi()
+  }
+  bindEvent() {
+    this.input.addEventListener('input', e => { this.inputHandler.call(this, e) })
+    window.addEventListener('click', e => { this.clickHandler.call(this, e) })
+    window.addEventListener('keyup', e => { this.escHandler.call(this, e) })
+  }
+  inputHandler(e) {
+    let value = this.handleInvalidateValue(e);
+    if (!value) return;
+    this.isTyping = true;
+    this.ul.classList.add('active')
+    this.handleLi(value)
+    this.ul.querySelector('li').classList.add('active')
+  }
+  handleLi(value = '') {
+    let html = '', _postfixList = this.postfixList;
+    let afterAt = value.split('@')[1];
+    let x = false;
+    if (afterAt) {
+      let regExp = new RegExp(`^${afterAt}`);
+      if (!(_postfixList.every(item => !regExp.test(item)))) {
+        _postfixList = [];
+        this.postfixList.forEach(item => {
+          if (regExp.test(item)) _postfixList.push(item.replace(afterAt, ''));
+        })
+      } else {
+        x = true
+      }
+    }
+    let s = `${value.split('@')[0]}@`;
+    _postfixList.forEach(item => {
+      html += `<li>${x ? s : value}${afterAt ? '' : '@'}${item}</li>`
+    })
+    this.ul.innerHTML = html;
+  }
+  handleInvalidateValue(e) {
+    let value = e.target.value.trim().replace(/(^\s+)|(\s+$)|\s+/g, '');
+    if (value === '') return this.removeActive();
+    // 注意实体字符转码
+    return value.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
+  }
+  removeActive(e) {
+    this.ul.classList.remove('active')
+    this.isTyping = false;
+  }
+  escHandler(e) {
+    if (e.key === 'Escape') {
+      this.removeActive()
+      this.input.select()
+    }
+    if (this.isTyping) {
+      let activeLi = [...this.ul.querySelectorAll('li')].filter(li => li.classList.contains('active'))[0];
+      if (e.key === 'ArrowDown') {
+        let nextLi = activeLi.nextElementSibling;
+        if (!nextLi) {
+          this.ul.firstElementChild.classList.add('active')
+        } else {
+          nextLi.classList.add('active')
+        }
+        activeLi.classList.remove('active');
+      }
+      if (e.key === 'ArrowUp') {
+        let previousLi = activeLi.previousElementSibling;
+        if (!previousLi) {
+          this.ul.lastElementChild.classList.add('active')
+        } else {
+          previousLi.classList.add('active')
+        }
+        activeLi.classList.remove('active')
+      }
+      if (e.key === 'Enter') {
+        this.input.value = activeLi.textContent;
+        this.removeActive()
+      }
+    }
+  }
+  clickHandler(e) {
+    let target = e.target;
+    if (target === this.input) return; // 点击自身
+    let isClickLi = [...document.querySelectorAll('#email-sug-wrapper li')].some(li => li === target);
+    // 实体字符的解码
+    isClickLi && (this.input.value = target.textContent.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&'));
+    this.removeActive()
+    this.input.focus()
+  }
+}
+new InputHandler()
