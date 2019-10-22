956ec2145ed09db07f77359694e58654b23f73bb
har3dy <har3dy@icloud.com>
2019-10-22 16:17:35 +0800

    优化代码结构

--- lesson/lesson-3-input-email.js
+++ lesson/lesson-3-input-email.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   document.getElementById('email-input').focus()
+  new InputHandler()
 }
 class InputHandler {
   constructor() {
@@ -7,17 +8,14 @@ class InputHandler {
     this.input = document.getElementById('email-input'),
       this.ul = document.getElementById('email-sug-wrapper');
     this.isTyping = false,
-      this.value = '',
-      this.selectedLiIndex = 0;
-    this.init()
-  }
-  init() {
+      this.value = '', // input的value值
+      this.selectedLiIndex = 0; // 提示框中选中的项的index
     this.bindEvent()
   }
   bindEvent() {
     this.input.addEventListener('input', e => { this.inputHandler.call(this, e) })
     window.addEventListener('click', e => { this.clickHandler.call(this, e) })
-    window.addEventListener('keyup', e => { this.escHandler.call(this, e) })
+    window.addEventListener('keyup', e => { this.keyupHandler.call(this, e) })
   }
   inputHandler(e) {
     this.value = this.handleInvalidateValue(e);
@@ -47,52 +45,49 @@ class InputHandler {
     })
     this.ul.innerHTML = html;
     [...this.ul.querySelectorAll('li')][this.selectedLiIndex].classList.add('active')
-    return html;
   }
-  handleInvalidateValue(e) {
+  handleInvalidateValue(e) { // 校验输入值是否有效
     let value = e.target.value.trim().replace(/(^\s+)|(\s+$)|\s+/g, '');
     if (value === '') return this.removeActive();
     // 注意实体字符转码
     return value.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/&/g, '&amp;');
   }
-  removeActive(e) {
+  removeActive() {
     this.ul.classList.remove('active')
     this.isTyping = false;
   }
-  escHandler(e) {
-    if (e.key === 'Escape') {
-      this.removeActive()
-      this.input.select()
-    }
-    if (this.isTyping) {
-      if (e.key === 'ArrowDown') {
-        this.selectedLiIndex++;
-        if (this.selectedLiIndex === [...this.ul.children].length) {
-          this.selectedLiIndex = 0;
-        }
-        this.handleLi(this.value)
-      }
-      if (e.key === 'ArrowUp') {
-        this.selectedLiIndex--;
-        if (this.selectedLiIndex < 0) {
-          this.selectedLiIndex = [...this.ul.children].length - 1;
-        }
-        this.handleLi(this.value)
-      }
-      if (e.key === 'Enter') {
-        this.input.value = [...this.ul.children][this.selectedLiIndex].textContent;
-        this.removeActive()
-      }
-    }
+  keyupHandler(e) {
+    if (e.key === 'Escape') return this.escHandler();
+    if (!this.isTyping) return;
+    if (e.key === 'ArrowDown') return this.arrowDownHandler()
+    if (e.key === 'Enter') return this.enterHandler()
+    if (e.key === 'ArrowUp') return this.arrowUpHandler()
+  }
+  escHandler() {
+    this.removeActive()
+    this.input.select()
+  }
+  arrowDownHandler() {
+    this.selectedLiIndex++;
+    if (this.selectedLiIndex === [...this.ul.children].length) this.selectedLiIndex = 0;
+    this.handleLi(this.value)
+  }
+  arrowUpHandler() {
+    this.selectedLiIndex--;
+    if (this.selectedLiIndex < 0) this.selectedLiIndex = [...this.ul.children].length - 1;
+    this.handleLi(this.value)
+  }
+  enterHandler() {
+    this.input.value = [...this.ul.children][this.selectedLiIndex].textContent;
+    this.removeActive()
   }
   clickHandler(e) {
     let target = e.target;
-    if (target === this.input) return; // 点击自身
+    if (target === this.input) return; // 点击input自身
     let isClickLi = [...document.querySelectorAll('#email-sug-wrapper li')].some(li => li === target);
-    // 实体字符的解码
+    // 实体字符需解码
     isClickLi && (this.input.value = target.textContent.replace('&lt;', '<').replace('&gt;', '>').replace('&amp;', '&'));
     this.removeActive()
     this.input.focus()
   }
 }
-new InputHandler()
