6eaac75898ac73939f36f46cefc9f28045a9e2f4
har3dy <har3dy@icloud.com>
2019-10-23 18:52:29 +0800

    使用职责链模式优化多选代码 并修复bug

--- js/utils.js
+++ js/utils.js
@@ -331,6 +331,19 @@ const utils = {
       this.removeActive()
       this.input.focus()
     }
+  },
+  Chain: class {
+    constructor(fn) {
+      this.fn = fn;
+    }
+    setNextSuccessor(nextSuccessor) {
+      return this.nextSuccessor = nextSuccessor;
+    }
+    passRequest() {
+      let ret = this.fn.apply(this, arguments)
+      if (ret === false) return this.nextSuccessor && this.nextSuccessor.passRequest.apply(this.nextSuccessor, arguments);
+      return ret;
+    }
   }
 }
 
--- lesson/lesson-4.js
+++ lesson/lesson-4.js
@@ -1,106 +1,39 @@
-class Chain {
-  constructor(fn) {
-    this.fn = fn;
-    this.nextSuccessor = null;
-  }
-  setNextSuccessor(nextSuccessor) {
-    return this.nextSuccessor = nextSuccessor;
-  }
-  passRequest() {
-    let ret = this.fn.apply(this, arguments)
-    if (ret === false) {
-      return this.nextSuccessor && this.nextSuccessor.passRequest.apply(this.nextSuccessor, arguments)
-    }
-    return ret;
-  }
-}
-window.onload = function () {
-  bindEvent()
-  chainBothSelect.passRequest({ selectedProduct, selectedRegion })
-}
-
-let regionSelect = document.getElementById('regionSelect'),
-  productSelect = document.getElementById('productSelect'),
-  selectedRegion = regionSelect.value,
-  selectedProduct = productSelect.value,
+import utils from '../js/utils.js'
+let { Chain } = utils;
+let selectForm = document.getElementById('selectForm');
+let regionList = [],
+  productList = [],
   html = '';
-
-function selectRegion({ selectedProduct, selectedRegion }) {
-  if (!(selectedRegion && !selectedProduct)) return false;
-  sourceData.forEach(item => item.region === selectedRegion && (html = handleDOM(item)))
-  return updateDOM()
-}
-function selectProduct({ selectedProduct, selectedRegion }) {
-  if (!(selectedProduct && !selectedRegion)) return false;
-  sourceData.forEach(item => item.product === selectedProduct && (html = handleDOM(item)))
-  return updateDOM()
-}
-function bothSelect({ selectedProduct, selectedRegion }) {
-  if (!(selectedRegion && selectedProduct)) return false;
-  sourceData.forEach(item => item.region === selectedRegion && item.product === selectedProduct && (html = handleDOM(item)))
-  return updateDOM()
-}
-function nullSelect() {
-  sourceData.forEach(item => html = handleDOM(item))
-  return updateDOM()
-}
-
-let selectForm = document.getElementById('selectForm')
-let regionList = productList = []
 selectForm.addEventListener('change', function (e) {
-  let target = e.target,
-    name = target.name;
-  if (name === 'regionSelect') {
-    regionList = handleSelectedList(this.regionSelect);
-    xx()
-  }
-  if (name === 'productSelect') {
-    productList = handleSelectedList(this.productSelect);
-    xx()
-  }
+  let name = e.target.name;
+  if (name === 'regionSelect') regionList = updateSelectedList(this.regionSelect);
+  if (name === 'productSelect') productList = updateSelectedList(this.productSelect);
+  triggerUpdate()
 })
-function xx() {
-  sourceData.forEach(item => {
-    if (regionList.includes(item.region) && productList.includes(item.product)) {
-      html = handleDOM(item)
-    }
-    if (regionList.includes(item.region) && !productList.includes(item.product)) {
-      html = handleDOM(item)
-    }
-    if (!regionList.includes(item.region) && productList.includes(item.product)) {
-      html = handleDOM(item)
-    }
-  })
+function triggerUpdate() {
+  sourceData.forEach(item => chainSelectBoth.passRequest(item))
   return updateDOM()
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let chainSelectBoth = new Chain(bothSelect), chainSelectRegion = new Chain(selectRegion), chainSelectProduct = new Chain(selectProduct);
+chainSelectBoth.setNextSuccessor(chainSelectRegion).setNextSuccessor(chainSelectProduct)
+function bothSelect(item) {
+  if (regionList.includes(item.region) && productList.includes(item.product)) return html = handleDOM(item);
+  return false;
+}
+function selectRegion(item) {
+  if (regionList.includes(item.region) && !productList.length) return html = handleDOM(item);
+  return false;
+}
+function selectProduct(item) {
+  if (productList.includes(item.product) && !regionList.length) return html = handleDOM(item);
+  return false;
+}
 // --------------------------------------------------------------------------
-function handleSelectedList(domList) {
+function updateSelectedList(domList) {
   let arr = [...domList].filter(item => item.checked);
   return arr.map(item => item.value)
 }
-let chainBothSelect = new Chain(bothSelect),
-  chainSelectProduct = new Chain(selectProduct),
-  chainSelectRegion = new Chain(selectRegion),
-  chainNullSelect = new Chain(nullSelect);
-chainBothSelect.setNextSuccessor(chainSelectProduct).setNextSuccessor(chainSelectRegion).setNextSuccessor(chainNullSelect)
 
 function updateDOM() {
   document.getElementById('tableWrapper').querySelector('tbody').innerHTML = html;
@@ -120,14 +53,4 @@ function handleDOM(item) {
   return html;
 }
 
-function bindEvent() {
-  regionSelect.addEventListener('change', e => {
-    selectedRegion = e.target.value;
-    chainBothSelect.passRequest({ selectedProduct, selectedRegion })
-  })
-  productSelect.addEventListener('change', e => {
-    selectedProduct = e.target.value;
-    chainBothSelect.passRequest({ selectedProduct, selectedRegion })
-  })
-}
 
