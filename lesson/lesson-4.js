f1ba41ed5ddc481147148c757a783ea81735ee8c
har3dy <har3dy@icloud.com>
2019-10-24 10:32:27 +0800

    使用mvc初次重构代码

--- lesson/lesson-4.js
+++ lesson/lesson-4.js
@@ -1,14 +1,79 @@
 import utils from '../js/utils.js'
 let { Chain } = utils;
-let regionList = ['华东'],
-  productList = [],
-  html = '';
-let regionAllSelect = document.getElementById('regionAllSelect'),
-  productAllSelect = document.getElementById('productAllSelect');
 window.onload = function () {
-  triggerUpdate()
+  controller.triggerUpdate()
 }
-regionAllSelect.addEventListener('click', function (e) {
+let view = {
+  regionDOMLists: [...document.getElementsByName('regionSelect')],
+  productDOMLists: [...document.getElementsByName('productSelect')],
+  regionAllSelect: document.getElementById('regionAllSelect'),
+  productAllSelect: document.getElementById('productAllSelect')
+}
+let model = {
+  regionList: ['华东'],
+  productList: [],
+  html: ''
+}
+
+let controller = {
+  handleEvent(e) {
+    if (this.name === 'regionSelect') controller.handleRegion.call(this, e, this.value);
+    if (this.name === 'productSelect') controller.handleProduct.call(this, e, this.value);
+    controller.triggerUpdate()
+  },
+  triggerUpdate() {
+    sourceData.forEach(item => chainSelectBoth.passRequest(item))
+    return controller.updateDOM()
+  },
+  handleRegion(e, value) {
+    if (model.regionList.length === 1 && model.regionList.includes(value) && model.productList.length === 0) {
+      return e.preventDefault()
+    }
+    model.regionList = controller.updateSelectedList(view.regionDOMLists);
+    view.regionAllSelect.checked = model.regionList.length === 3 ? true : false;
+    return model.regionList;
+  },
+  handleProduct(e, value) {
+    if (model.productList.length === 1 && model.productList.includes(value) && model.regionList.length === 0) {
+      return e.preventDefault()
+    }
+    model.productList = controller.updateSelectedList(view.productDOMLists);
+    view.productAllSelect.checked = model.productList.length === 3 ? true : false;
+    return model.productList;
+  },
+  bindEvent(domLis, cb) {
+    domLis.forEach(item => {
+      item.addEventListener('click', function (e) {
+        cb.call(this, e)
+      })
+    })
+  },
+  updateSelectedList(domList) {
+    let arr = domList.filter(item => item.checked);
+    return arr.map(item => item.value)
+  },
+  handleDOM(item) {
+    let saleHTML = '',
+      html = model.html;
+    item.sale.forEach(s => {
+      saleHTML += `<td>${s}</td>`
+    })
+    html += `<tr>
+          <td>${item.region}</td>
+          <td>${item.product}</td>
+          ${saleHTML}
+        </tr>`
+    return model.html = html;
+  },
+  updateDOM() {
+    document.getElementById('tableWrapper').querySelector('tbody').innerHTML = model.html;
+    return model.html = ''
+  },
+}
+// 绑定事件
+controller.bindEvent(view.regionDOMLists, controller.handleEvent)
+controller.bindEvent(view.productDOMLists, controller.handleEvent)
+controller.bindEvent([view.regionAllSelect], function (e) {
   if (this.checked) {
     [...document.getElementsByName('regionSelect')].forEach(item => {
       if (!item.checked) {
@@ -17,7 +82,7 @@ regionAllSelect.addEventListener('click', function (e) {
     })
   }
 })
-productAllSelect.addEventListener('click', function (e) {
+controller.bindEvent([view.productAllSelect], function (e) {
   if (this.checked) {
     e.preventDefault();
     [...document.getElementsByName('productSelect')].forEach(item => {
@@ -27,80 +92,21 @@ productAllSelect.addEventListener('click', function (e) {
     })
   }
 })
-// 处理必须保留一个选择的函数
-function handleRegion(e, value) {
-  if (regionList.length === 1 && regionList.includes(value) && productList.length === 0) {
-    return e.preventDefault()
+
+let chainNodeFn = {
+  bothSelect(item) {
+    if (model.regionList.includes(item.region) && model.productList.includes(item.product)) return model.html = controller.handleDOM(item);
+    return false;
+  },
+  selectRegion(item) {
+    if (model.regionList.includes(item.region) && !model.productList.length) return model.html = controller.handleDOM(item);
+    return false;
+  },
+  selectProduct(item) {
+    if (model.productList.includes(item.product) && !model.regionList.length) return model.html = controller.handleDOM(item);
+    return false;
   }
-  regionList = updateSelectedList(regionLis);
-  regionAllSelect.checked = regionList.length === 3 ? true : false;
-  return regionList;
-}
-function handleProduct(e, value) {
-  if (productList.length === 1 && productList.includes(value) && regionList.length === 0) {
-    return e.preventDefault()
-  }
-  productList = updateSelectedList(productLis);
-  productAllSelect.checked = productList.length === 3 ? true : false;
-  return productList;
-}
-// 更新列表后触发更新dom
-function triggerUpdate() {
-  sourceData.forEach(item => chainSelectBoth.passRequest(item))
-  return updateDOM()
 }
 // 职责链节点函数
-let chainSelectBoth = new Chain(bothSelect), chainSelectRegion = new Chain(selectRegion), chainSelectProduct = new Chain(selectProduct);
+let chainSelectBoth = new Chain(chainNodeFn.bothSelect), chainSelectRegion = new Chain(chainNodeFn.selectRegion), chainSelectProduct = new Chain(chainNodeFn.selectProduct);
 chainSelectBoth.setNextSuccessor(chainSelectRegion).setNextSuccessor(chainSelectProduct)
-function bothSelect(item) {
-  if (regionList.includes(item.region) && productList.includes(item.product)) return html = handleDOM(item);
-  return false;
-}
-function selectRegion(item) {
-  if (regionList.includes(item.region) && !productList.length) return html = handleDOM(item);
-  return false;
-}
-function selectProduct(item) {
-  if (productList.includes(item.product) && !regionList.length) return html = handleDOM(item);
-  return false;
-}
-// --------------------------------------------------------------------------
-// let view = {
-//   regionLis: [...document.getElementsByName('regionSelect')]
-// }
-let regionLis = [...document.getElementsByName('regionSelect')],
-  productLis = [...document.getElementsByName('productSelect')];
-bindEvent(regionLis)
-bindEvent(productLis)
-function bindEvent(domLis) {
-  domLis.forEach(li => {
-    li.addEventListener('click', function (e) {
-      if (this.name === 'regionSelect') handleRegion.call(this, e, this.value);
-      if (this.name === 'productSelect') handleProduct.call(this, e, this.value);
-      triggerUpdate()
-    })
-  })
-}
-function updateSelectedList(domList) {
-  let arr = domList.filter(item => item.checked);
-  return arr.map(item => item.value)
-}
-
-// 最后一步
-function updateDOM() {
-  document.getElementById('tableWrapper').querySelector('tbody').innerHTML = html;
-  return html = ''
-}
-
-function handleDOM(item) {
-  let saleHTML = ''
-  item.sale.forEach(s => {
-    saleHTML += `<td>${s}</td>`
-  })
-  html += `<tr>
-          <td>${item.region}</td>
-          <td>${item.product}</td>
-          ${saleHTML}
-        </tr>`
-  return html;
-}
