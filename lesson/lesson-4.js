826ee4ff6688a64bdab8fcfb2bd1311219fa78c2
har3dy <har3dy@icloud.com>
2019-10-24 13:07:54 +0800

    重写全选功能

--- lesson/lesson-4.js
+++ lesson/lesson-4.js
@@ -6,13 +6,19 @@ window.onload = function () {
 let checkboxView = {
   regionCheckboxWrapper: document.getElementById('regionCheckbox'),
   productCheckboxWrapper: document.getElementById('productCheckbox'),
-  regionDOMLists: [...document.getElementsByName('regionSelect')],
-  productDOMLists: [...document.getElementsByName('productSelect')]
+  regionDOMLists: [...document.querySelectorAll('#regionCheckbox input')],
+  productDOMLists: [...document.querySelectorAll('#productCheckbox input')],
+  regionAllSelect: document.getElementById('regionAllSelect'),
+  productAllSelect: document.getElementById('productAllSelect')
 }
 let checkboxModel = {
   regionList: ['华东'], // 已选的地区
   productList: [], // 已选的商品
-  html: '' // 将要更新的视图html
+  html: '', // 将要更新的视图html
+  getLowercaseName(elem) {
+    let regExp = new RegExp(/(.*)[A-Z]/);
+    return regExp.exec(elem.name)[1];
+  }
 }
 
 let checkboxController = {
@@ -21,9 +27,8 @@ let checkboxController = {
   },
   handleCheckboxClickEvent(e) { // 第2步，根据不同name传入不同的参数
     let elem = e.target;
-    if (elem.tagName.toLowerCase() !== 'input') return; // 会先点到label上
-    let name = elem.name, regExp = new RegExp(/(.*)[A-Z]/);
-    name = regExp.exec(name)[1]; // 拿到第一个大写字母之前的单词
+    if (elem.tagName.toLowerCase() !== 'input') return; // 会先点到label上，是label就返回
+    let name = checkboxModel.getLowercaseName(elem);
     let params = {
       value: elem.value,
       list_1: `${name}List`,
@@ -31,6 +36,7 @@ let checkboxController = {
     }
     if (!checkboxController.checkValidate.call(elem, e, params)) return; // 检测是否只选择了一个
     checkboxModel[`${name}List`] = checkboxController.updateSelectedList(checkboxView[`${name}DOMLists`]); // 更新本地选择列表
+    checkboxController.allSelect(checkboxView[`${name}DOMLists`], checkboxView[`${name}AllSelect`]) // 处理全选
     checkboxController.triggerUpdate()
   },
   checkValidate(e, { value, list_1, list_2 }) { // 第3步，检测是否能够更新列表，如果两个列表中只有一个数据，则阻止默认事件
@@ -42,6 +48,17 @@ let checkboxController = {
   updateSelectedList(dom) { // 第4步，更新本地被选列表
     let arr = dom.filter(item => item.checked);
     return arr.map(item => item.value)
+  },
+  allSelect(dom, allSelectDom) {
+    let a = dom.every(item => item.checked);
+    allSelectDom.checked = a ? true : false;
+  },
+  handleAllSelectClicked(e) {
+    let elem = e.target;
+    if (!elem.checked) return elem.checked = true;
+    let name = checkboxModel.getLowercaseName(elem)
+    let unclickedInputList = checkboxView[`${name}DOMLists`].filter(item => !item.checked)
+    unclickedInputList.length !== 0 && unclickedInputList.forEach(item => item.click())
   },
   triggerUpdate() { // 第5步，根据本地列表筛选item并生成html，
     sourceData.forEach(item => chainSelectBoth.passRequest(item)) // 内嵌了generateDOM函数
@@ -68,6 +85,8 @@ let checkboxController = {
 // 绑定事件
 checkboxController.bindEvent({ dom: checkboxView.regionCheckboxWrapper, callback: checkboxController.handleCheckboxClickEvent })
 checkboxController.bindEvent({ dom: checkboxView.productCheckboxWrapper, callback: checkboxController.handleCheckboxClickEvent })
+checkboxController.bindEvent({ dom: checkboxView.regionAllSelect, callback: checkboxController.handleAllSelectClicked })
+checkboxController.bindEvent({ dom: checkboxView.productAllSelect, callback: checkboxController.handleAllSelectClicked })
 // 职责链节点函数
 let chainNodeFn = {
   bothSelect(item) {
